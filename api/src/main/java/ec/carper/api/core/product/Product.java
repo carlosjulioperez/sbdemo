c049c931827458a8680211f728e1a3cd8d98960a
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2021-02-24 16:44:52 -0500

    Agregar POJOS

--- api/src/main/java/ec/carper/api/core/product/Product.java
+++ api/src/main/java/ec/carper/api/core/product/Product.java
@@ -1,3 +1,40 @@
-package ec.carper.core.product;
+package ec.carper.api.core.product;
 
+public class Product{
 
+    private final int productId;
+    private final String nombre;
+    private final String descripcion;
+    private final String serviceAddress;
+
+    public Product() {
+		this.productId = 0;
+		this.nombre = null;
+		this.descripcion = null;
+		this.serviceAddress = null;
+	}
+
+	public Product(int productId, String nombre, String descripcion, String serviceAddress) {
+		this.productId = productId;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.serviceAddress = serviceAddress;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public String getServiceAddress() {
+		return serviceAddress;
+	}
+
+}
