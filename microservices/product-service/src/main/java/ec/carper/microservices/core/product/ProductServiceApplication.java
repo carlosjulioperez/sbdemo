f38038dcfc885165cc4d37aa0f114bd31609f802
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2021-02-24 21:59:07 -0500

    Integración del proyecto

--- microservices/product-service/src/main/java/ec/carper/microservices/core/product/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/ec/carper/microservices/core/product/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package ec.carper.microservices.core.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("ec.carper")
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
---
+++ microservices/product-service/src/main/java/ec/carper/microservices/core/product/services/ProductServiceImpl.java
@@ -0,0 +1,38 @@
+package ec.carper.microservices.core.product.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import ec.carper.api.core.product.Product;
+import ec.carper.api.core.product.ProductService;
+import ec.carper.util.exceptions.InvalidInputException;
+import ec.carper.util.exceptions.NotFoundException;
+import ec.carper.util.http.ServiceUtil;
+
+@RestController
+public class ProductServiceImpl implements ProductService{
+		
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
+
+    private final ServiceUtil serviceUtil;
+
+    @Autowired
+    public ProductServiceImpl(ServiceUtil serviceUtil) {
+        this.serviceUtil = serviceUtil;
+    }
+
+    @Override
+    public Product getProduct(int productId) {
+        LOG.debug("/product return the found product for productId={}", productId);
+
+        if (productId < 1) throw new InvalidInputException("Invalid productId: " + productId);
+
+        if (productId == 13) throw new NotFoundException("No product found for productId: " + productId);
+
+        return new Product(productId, "nombre-" + productId, "descripcion-" + productId, serviceUtil.getServiceAddress());
+    }
+
+}
